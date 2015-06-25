97370752390925dafd434e6e67f3d3654a4f0949
kevin <iscarryon@gmail.com>
2015-06-25 19:22:41 +0800

    kevin: commit for phonegap

---
+++ pages/teachers/TeacherCtrl.js
@@ -0,0 +1,30 @@
+﻿app.controller('TeacherCtrl', ['$state', '$scope', 'TeacherClintService', function ($state, $scope, TeacherClintService) {
+
+    TeacherClintService.getSubject().then(function (data) {
+
+        $scope.SubjectList = data;
+    })
+    debugger;
+    TeacherClintService.getAllCarousel("師資團隊").then(function (data) {
+
+        debugger;
+        $scope.DataList = data;
+    });
+
+    
+
+
+    $scope.goToTeachersDetail = function (ppId) {
+        $state.go('app.teacherDetail', { id: ppId});
+    }
+   
+}]);
+
+app.controller('TeacherDetailCtrl', ['$state', '$scope', '$stateParams', 'TeacherClintService', '$sce', function ($state, $scope, $stateParams, TeacherClintService, $sce) {
+    TeacherClintService.getTeacherBySubject($stateParams.id).then(function (data) {
+        
+        $scope.TeacherList = data;
+    })
+    $scope.trustAsHtml = $sce.trustAsHtml;
+
+}]);
